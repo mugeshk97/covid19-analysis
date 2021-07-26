dfde285b8fc10a5cf6618b3eda525267bf46bccc
Mugesh Kannan <mugeshk1171@gmail.com>
2021-07-26 16:03:08 +0530

    Update exploration.sql

--- exploration.sql
+++ exploration.sql
@@ -19,7 +19,7 @@ order by 1,2 asc;
  
 select date, location, population, total_cases, (total_cases/ population)*100 as InfectedPercent
 from covid_19.coronadeath
-Where location like '%Russ%'
+where location like '%Russ%'
 order by 1,2 asc;
 
 -- Country with high infection rate compared to population
@@ -31,9 +31,45 @@ order by 4 desc;
 
 -- Countries with highest death count per population
 
-select location, max(cast(total_deaths as int)) as MaxTotalDeath
+select location, max(cast(total_deaths as float)) as MaxTotalDeath
 from covid_19.coronadeath
 group by location
 order by 2 desc;
+
+
+-- Gobal death percentage to infected 
+
+select date, sum(new_cases) as total_cases, sum(cast(new_deaths as float)) as total_deaths, sum(cast(new_deaths as float))/ sum(new_cases)*100 as DeathPercent
+from covid_19.coronadeath  
+group by date
+order by 1;
+
+select sum(new_cases) as total_cases, sum(cast(new_deaths as float)) as total_deaths, sum(cast(new_deaths as float))/ sum(new_cases)*100 as DeathPercent
+from covid_19.coronadeath;
+
+-- Let's Explore info about vaccinations
+-- Total Population vs vaccination 
+
+
+
+select dae.location,dae.date, dae.population, vac.new_vaccinations, 
+sum(vac.new_vaccinations) over (partition by dae.location order by dae.location, dae.date) as RollingPeopleVaccinated
+from covid_19.coronadeath dae join covid_19.coronavaccine vac  
+on dae.location = vac.location
+and dae.date = vac.date
+order by 1,2;
+
+-- Use CTE to calculate RollingPeopleVaccinated percentage to population
+
+with Popvsvac (location, date, population, new_vaccinations, RollingPeopleVaccinated)
+as (
+	select dae.location, dae.date, dae.population, vac.new_vaccinations, 
+	sum(vac.new_vaccinations) over (partition by dae.location order by dae.location, dae.date) as RollingPeopleVaccinated
+	from covid_19.coronadeath dae join covid_19.coronavaccine vac  
+	on dae.location = vac.location
+	and dae.date = vac.date
+)
+select *, round((RollingPeopleVaccinated/population)*100, 3) as Percent
+From Popvsvac
 
  
